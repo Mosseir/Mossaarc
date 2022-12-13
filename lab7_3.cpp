0a5fd2319360f0c522b93f30bcabc2a9c401203f
Mosseir <glock18_123@hotmail.com>
2022-12-13 17:37:08 +0700

    finished

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
+ int adiff(int A,int B)
+ {
+       int C , D ;
+       if(A<B){
+        C = B-A;
+        D = C%360;
+        if(D>180){
+          return 360-D;
+        }else{
+          return D;
+        }
+       }else{
+        C = A-B;
+        D = C%360; 
+        }
+        if(D>180){
+          return 360-D;
+        }else{
+          return D;
+       }
+}
 
 int main(){
   cout << adiff(180,270);
@@ -12,4 +34,6 @@ int main(){
   cout << adiff(90,-90);
   cout << adiff(1000,280);
   cout << adiff(60,244);
+
+    return 0;
 }
